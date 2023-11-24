a911a78cfd68a67111af94dfc4164edc623abbb7
IDOKO EMMANUEL <eemmanuel.idoko@gmail.com>
2023-11-24 11:41:57 +0000

    transposing vectors using 2 dimensional arrays instead

--- pyFiles/columnVector.py
+++ pyFiles/columnVector.py
@@ -6,3 +6,9 @@ print(features)
 print(features.T)
 
 print(features[:, None])
+
+# Alternatively, you can create arrays with two dimensions. Then, you can use arr.T to get the column vector.
+
+np.array(features, ndmin=2)
+
+print(np.array(features, ndmin=2).T)
